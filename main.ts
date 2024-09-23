c3164f9f5baa8ba7679bcd1a55e1a0c5a5392db6
Nguyễn Quang Huy <129473045+huynguyenquangce@users.noreply.github.com>
2024-09-23 22:55:32 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,35 @@
-basic.clearScreen()
-basic.showString("Lab1")
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . # . . #
+            . # # # #
+            . # . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . # # .
+            . . # . #
+            . . # # .
+            . . # . #
+            . . # # .
+            `)
+    }
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
 basic.forever(function () {
-    basic.showIcon(IconNames.Diamond)
-    basic.pause(1000)
-    basic.showIcon(IconNames.Sword)
-    basic.pause(1000)
-    basic.showIcon(IconNames.Chessboard)
-    basic.pause(1000)
+    if (input.temperature() >= 30) {
+        led.plot(0, 0)
+    } else {
+        led.unplot(0, 0)
+    }
 })
