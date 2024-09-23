e8725b87ff60447fbcf121c54ea565ed964b0d0a
Nguyễn Quang Huy <129473045+huynguyenquangce@users.noreply.github.com>
2024-09-23 22:04:45 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
-basic.clearScreen()
-basic.showString("Lab1")
+let degree = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.compassHeading())
+})
 basic.forever(function () {
-    basic.showIcon(IconNames.Diamond)
-    basic.pause(1000)
-    basic.showIcon(IconNames.Sword)
-    basic.pause(1000)
-    basic.showIcon(IconNames.Chessboard)
-    basic.pause(1000)
+    degree = input.compassHeading()
+    if (degree >= 0 && degree < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (degree >= 45 && degree < 135) {
+        basic.showArrow(ArrowNames.East)
+    } else if (degree >= 135 && degree < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (degree >= 225 && degree < 315) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showArrow(ArrowNames.North)
+    }
 })
