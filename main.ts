ff3701cd05f7626114aeef7b83c218cd77a1e4a6
Nguyễn Quang Huy <129473045+huynguyenquangce@users.noreply.github.com>
2024-09-23 22:47:11 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,113 +1,23 @@
-let buttonPressed = 0
-input.onButtonPressed(Button.A, function () {
-    // Set A as pressed
-    buttonPressed = 1
-    basic.clearScreen()
-    while (buttonPressed == 1) {
-        // Run as long as A is pressed
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . # . . .
-            # . . . .
-            # # # # .
-            # . . . .
-            . # . . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            # . . . .
-            . . . . .
-            # # # . #
-            . . . . .
-            # . . . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . . . .
-            . . . . #
-            # # . # #
-            . . . . #
-            . . . . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            # . # # #
-            . . . # .
-            . . . . #
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . . # .
-            . . # . .
-            . # # # #
-            . . # . .
-            . . . # .
-            `)
-        basic.pause(100)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else {
+        basic.showArrow(ArrowNames.South)
     }
-})
-input.onButtonPressed(Button.B, function () {
-    // Set B as pressed
-    buttonPressed = 2
+    basic.pause(100)
     basic.clearScreen()
-    while (buttonPressed == 2) {
-        // Run as long as B is pressed
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . . # .
-            . . . . #
-            . # # # #
-            . . . . #
-            . . . # .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            # . # # #
-            . . . . .
-            . . . . #
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . . . . .
-            # . . . .
-            # # . # #
-            # . . . .
-            . . . . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            # . . . .
-            . # . . .
-            # # # . #
-            . # . . .
-            # . . . .
-            `)
-        basic.pause(100)
-        basic.showLeds(`
-            . # . . .
-            . . # . .
-            # # # # .
-            . . # . .
-            . # . . .
-            `)
-        basic.pause(100)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(0)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
+basic.forever(function () {
+    if (input.temperature() >= 30) {
+        led.plot(0, 0)
+    } else {
+        led.unplot(0, 0)
     }
 })
